d62631f0c84d2303e07015b1089faf68fdec0241
Pragesh Bhandari <124021101+prageshb@users.noreply.github.com>
2023-08-07 13:54:19 +0545

    Br.java

--- Br.java
+++ Br.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 public class Br {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the brightness you want to set: ");
         int value = sc.nextInt();
 
         java.io.File file = new java.io.File("/sys/class/backlight/intel_backlight/brightness");
@@ -15,9 +16,9 @@ public class Br {
         PrintWriter pw = new PrintWriter(fw);
         pw.println(value);
         pw.close();
-
+	
+	System.out.println("Done");
 
 
     }
 }
-
